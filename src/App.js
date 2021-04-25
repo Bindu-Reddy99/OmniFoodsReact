52205033a577738d27c6ea1b39c16d493f6f86d8
Bindu <paris@atlassian.com>
2021-04-25 08:39:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from "./components/Footer/Footer";
+import SignUp from "./components/SignUp/SignUp";
+import Delivery from "./components/Delivery/Delivery";
+import Header from "./components/Header/Header";
+import Meals from "./components/Meals/Meals";
+import Cities from "./components/Cities/Cities";
+import Pricing from "./components/Pricing/Pricing";
+import Feedbacks from "./components/Feedbacks/Feedbacks";
+import HowItWorks from "./components/HowItWorks/HowItWorks";
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Delivery />
+        <Meals />
+        <HowItWorks />
+        <Cities />
+        <Feedbacks />
+        <Pricing />
+        <SignUp />
+        <Footer />
+      </Fragment>
+    );
+  }
 }
-
-export default App;
